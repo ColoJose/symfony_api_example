4f9b2f552fbdc77699071c2f96f864239c568a87
colojose <msanchez.jose.94@gmail.com>
2021-04-10 22:34:20 -0300

    update property table

--- migrations/Version20210410223924.php
+++ migrations/Version20210410223924.php
@@ -20,7 +20,7 @@ final class Version20210410223924 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE property (id INT AUTO_INCREMENT NOT NULL, street VARCHAR(255) NOT NULL, number INT NOT NULL, zip_code INT DEFAULT NULL, sqm DOUBLE PRECISION NOT NULL, has_garden TINYINT(1) NOT NULL, location VARCHAR(255) NOT NULL, discr VARCHAR(255) NOT NULL, floors INT DEFAULT NULL, garden_sqm INT DEFAULT NULL, accept_pets TINYINT(1) DEFAULT NULL, is_loft TINYINT(1) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE property (id INT AUTO_INCREMENT NOT NULL, street VARCHAR(255) NOT NULL, number INT NOT NULL, zip_code INT DEFAULT NULL, sqm DOUBLE PRECISION NOT NULL, has_garden TINYINT(1) DEFAULT NULL, location VARCHAR(255) NOT NULL, discr VARCHAR(255) NOT NULL, floors INT DEFAULT NULL, garden_sqm INT DEFAULT NULL, accept_pets TINYINT(1) DEFAULT NULL, is_loft TINYINT(1) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
