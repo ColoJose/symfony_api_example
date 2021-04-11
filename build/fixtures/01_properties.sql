7fd8ce6f1a51cc04ce899cadc990e7ac412156ac
colojose <msanchez.jose.94@gmail.com>
2021-04-10 22:35:33 -0300

    add properties sample data

---
+++ build/fixtures/01_properties.sql
@@ -0,0 +1,11 @@
+-- insert flats
+insert into property (street, number, zip_code, sqm, location, discr, accept_pets, is_loft) values ('Portman St', 110, 6755,25.5,'Camden','flat',0,0);
+insert into property (street, number, zip_code, sqm, location, discr, accept_pets, is_loft) values ('Arlington Rd', 540, 6755,60.0,'Camden','flat',1,1);
+insert into property (street, number, zip_code, sqm, location, discr, accept_pets, is_loft) values ('Phoenix Rd', 998, 7410,21.0,'Islington','flat',0,1);
+insert into property (street, number, zip_code, sqm, location, discr, accept_pets, is_loft) values ('Werrington St', 52, 7410,40.9,'Islington','flat',1,0);
+insert into property (street, number, zip_code, sqm, location, discr, accept_pets, is_loft) values ('Portman St', 636, 5662,50.0,'Kensington','flat',0,1);
+-- insert houses
+insert into property (street, number, zip_code, sqm, location, discr, floors, has_garden, garden_sqm) values ('N Mews',789, 9902,60.0,'Haringey','house',1,0,10.5);
+insert into property (street, number, zip_code, sqm, location, discr, floors, has_garden, garden_sqm) values ('Roger St',614, 9902,70.0,'Haringey','house',2,0,8.0);
+insert into property (street, number, zip_code, sqm, location, discr, floors, has_garden) values ('Herbal hill',319, 6728,50.0,'Hackney','house',2,1);
+insert into property (street, number, zip_code, sqm, location, discr, floors, has_garden) values ('Summers St',562, 6728,35.0,'Hackney','house',3,1);
