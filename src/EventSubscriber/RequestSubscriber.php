d557b59ef551294617c751cabd23f88259d58218
colojose <msachez.jose.94@gmail.com>
2021-07-16 00:55:42 -0300

    add RequestSubscriber for request events

--- src/EventSubscriber/RequestSubscriber.php
+++ src/EventSubscriber/RequestSubscriber.php
@@ -1,17 +1,29 @@
 <?php
 
-
 namespace App\EventSubscriber;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 
+class RequestSubscriber implements EventSubscriberInterface {
 
-class RequestSubscriber implements \Symfony\Component\EventDispatcher\EventSubscriberInterface
-{
+    private $logger;
+
+    public function __construct(LoggerInterface $logger) {
+        $this->logger = $logger;
+    }
 
     /**
      * @inheritDoc
      */
-    public static function getSubscribedEvents()
-    {
-        // TODO: Implement getSubscribedEvents() method.
+    public static function getSubscribedEvents() {
+        return [
+            RequestEvent::class => 'onKernelRequest',
+        ];
+    }
+
+    public function onKernelRequest(RequestEvent $event) {
+        $this->logger->info(sprintf('Host: %s', $event->getRequest()->getHost()));
     }
 }
+
