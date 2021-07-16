12eca4164a34259deb128413e1b0bd1debce61e8
colojose <msachez.jose.94@gmail.com>
2021-07-15 23:11:02 -0300

    add symfony monolog bundle

---
+++ src/EventSubscriber/RequestSubscriber.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\EventSubscriber;
+
+
+class RequestSubscriber implements \Symfony\Component\EventDispatcher\EventSubscriberInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public static function getSubscribedEvents()
+    {
+        // TODO: Implement getSubscribedEvents() method.
+    }
+}
